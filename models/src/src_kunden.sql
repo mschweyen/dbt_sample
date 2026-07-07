d5b76d725e1c9ce8528699f6b23a9c08ef74fdb8
vbumaha <vbumaha@gmail.com>
2026-07-07 19:22:59 +0200

    test commit

---
+++ models/src/src_kunden.sql
@@ -0,0 +1,11 @@
+WITH raw_kunden AS (
+    SELECT
+        *
+    FROM
+        {{ ref('kunden') }}
+)
+SELECT
+    changedate            as change_date,
+    vrb_filial_und_betreuernr_an964 AS vrb_filial_und_betreuernr_an964
+FROM
+    raw_kunden
